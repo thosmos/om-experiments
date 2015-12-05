e5f301ca5bacad4dec3cbff06aadf1cfea745826
Thomas Spellman <thos37@gmail.com>
2015-12-05 15:59:27 -0800

    initial

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject om-tutorial-starter "0.1.0-SNAPSHOT"
+  :description "Empty Om Tutorial"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha25"]
+                 [figwheel-sidecar "0.5.0-1" :scope "test"]
+                 [datascript "0.13.2"]])
---
+++ src/om_tutorial/core.cljs
@@ -0,0 +1,9 @@
+(ns om-tutorial.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]
+            [datascript.core :as d]))
+
+(enable-console-print!)
+
+(println "Hello world!")
